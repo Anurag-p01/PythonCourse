b32920ee2943b5a16c6099bb1dc0b9bd427b0242
Anurag-p01 <anuragstu147@gmail.com>
2025-09-23 15:41:32 +0530

    add

---
+++ perfect or not.py
@@ -0,0 +1,7 @@
+n = int(input("Check your number is perfect or not : "))
+sum = 0
+for i in range(1, n):
+    if n % i == 0:
+        sum = sum + i
+if sum == n:        
+        print("Perfect Number")    
