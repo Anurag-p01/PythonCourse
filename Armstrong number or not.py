bd656f4a69852f7ec2e7b1974be523cdb4bc67f5
Anurag-p01 <anuragstu147@gmail.com>
2025-09-27 23:46:50 +0530

    added

--- Armstrong number or not.py
+++ Armstrong number or not.py
@@ -1,47 +1,17 @@
-# Python program to determine whether
-# the number is Armstrong number or not
+# Check if a number is an Armstrong number
 
-# Function to calculate x raised to 
-# the power y
-def power(x, y):
-    
-    if y == 0:
-        return 1
-    if y % 2 == 0:
-        return power(x, y // 2) * power(x, y // 2)
-        
-    return x * power(x, y // 2) * power(x, y // 2)
+# Get input from user
+num = int(input("Enter a number: "))
 
-# Function to calculate order of the number
-def order(x):
+# Convert number to string to count digits
+num_str = str(num)
+num_digits = len(num_str)
 
-    # Variable to store of the number
-    n = 0
-    while (x != 0):
-        n = n + 1
-        x = x // 10
-        
-    return n
+# Calculate the sum of digits raised to the power of number of digits
+sum_of_powers = sum(int(digit) ** num_digits for digit in num_str)
 
-# Function to check whether the given 
-# number is Armstrong number or not
-def isArmstrong(x):
-    
-    n = order(x)
-    temp = x
-    sum1 = 0
-    
-    while (temp != 0):
-        r = temp % 10
-        sum1 = sum1 + power(r, n)
-        temp = temp // 10
-
-    # If condition satisfies
-    return (sum1 == x)
-
-# Driver code
-x = 153
-print(isArmstrong(x))
-
-x = 1253
-print(isArmstrong(x))
+# Check if the number is an Armstrong number
+if sum_of_powers == num:
+    print(f"{num} is an Armstrong number.")
+else:
+    print(f"{num} is not an Armstrong number.")
